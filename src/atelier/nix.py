346e9adf8e233dcdae17532749a7c7f88a719b6f
Yifei Sun <ysun@hey.com>
2026-08-16 22:13:19 +0200

    nix: drop nix-eval-jobs override

--- src/atelier/nix.py
+++ src/atelier/nix.py
@@ -186,11 +186,7 @@ def _eval_command(
     cmd = [
         "nix",
         "run",
-        # nixpkgs#nix-eval-jobs 2.35.0 cache status resolver bug
-        # https://github.com/NixOS/nix-eval-jobs/issues/430
-        # temporary override drop after fix
-        "github:stepbrobd/inc#nix-eval-jobs",
-        # "nixpkgs#nix-eval-jobs",
+        "nixpkgs#nix-eval-jobs",
         "--",
         "--flake",
         flake,
